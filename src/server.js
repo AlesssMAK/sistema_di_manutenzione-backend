0ff200e0c6886aa3fff58b04dc6ceec0c74bffa2
Alexey <makovii88@gmail.com>
2026-01-30 18:38:53 +0100

    added add express structure and error handling

---
+++ src/server.js
@@ -0,0 +1,34 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { logger } from './middleware/logger.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errors } from 'celebrate';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import helmet from 'helmet';
+
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+app.use(helmet());
+app.use(logger);
+app.use(express.json());
+app.use(
+  cors({
+    origin: ['https://http://localhost:3000/'],
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
+
+app.use(notFoundHandler);
+app.use(errors());
+app.use(errorHandler);
+
+await connectMongoDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
