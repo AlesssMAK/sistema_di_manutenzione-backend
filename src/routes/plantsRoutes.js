f7a6d907a43b0c676626492907dc84d6d86a76d3
Iryna-M-html <ivmohnataya@gmail.com>
2026-02-05 11:41:10 +0200

    create validation for plant

--- src/routes/plantsRoutes.js
+++ src/routes/plantsRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { createPlantSchema } from '../validations/plantValidation.js';
 const router = Router();
 
 router.post('/plants', celebrate(createPlantSchema), createPlant);
