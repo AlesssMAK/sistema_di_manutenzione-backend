24b69588e8b1025aa197bfca65b411c17c882ff0
Iryna-M-html <ivmohnataya@gmail.com>
2026-02-09 11:41:47 +0200

    create partPlant model

---
+++ src/models/part.js
@@ -0,0 +1,23 @@
+import { Schema } from 'mongoose';
+import { model } from 'mongoose';
+
+const partPlantSchema = new Schema(
+  {
+    namePartPlant: { type: String, required: true },
+    codePartPlant: { type: String, required: true },
+    location: { type: String, required: true },
+    description: { type: String },
+  },
+  {
+    timestamps: true,
+  },
+);
+partPlantSchema.index(
+  {
+    namePartPlant: 'text',
+    description: 'text',
+  },
+  { namePartPlant: 'PartPlantTextIndex' },
+);
+
+export const PartPlant = model('PartPlant', partPlantSchema);
