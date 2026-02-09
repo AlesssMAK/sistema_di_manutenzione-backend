c69e405f4464473f234cbcc101d11dbd3982cd6c
Iryna-M-html <ivmohnataya@gmail.com>
2026-02-09 12:23:29 +0200

    create partplantsController

---
+++ src/routes/partplantRoutes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { createPartPlantSchema } from '../validations/partplantValidation.js';
+import { celebrate } from 'celebrate';
+import { authenticate } from '../middleware/authenticate.js';
+import { requireAdmin } from '../middleware/requireAdmin.js';
+import { createPartPlant } from '../controllers/partPlantController.js';
+const router = Router();
+router.post(
+  '/plants/:id/parts',
+  authenticate,
+  requireAdmin,
+  celebrate(createPartPlantSchema),
+  createPartPlant,
+);
+export default router;
--- src/validations/partplantValidation.js
+++ src/validations/partplantValidation.js
@@ -4,14 +4,16 @@ export const createPartPlantSchema = {
     plants: Joi.array()
       .items(
         Joi.object({
-          plantId: Joi.string().required(),
+          plantId: Joi.string()
+            .regex(/^[0-9a-fA-F]{24}$/)
+            .required(),
         }),
       )
       .min(1)
       .required(),
+    namePartPlant: Joi.string().trim().min(4).required(),
+    codePartPlant: Joi.string().trim().min(4).required(),
+    location: Joi.string().min(4).trim().required(),
+    description: Joi.string().trim().min(4).allow('', null).optional(),
   }),
-  namePartPlant: Joi.string().trim().min(4).required(),
-  codePartPlant: Joi.string().trim().min(4).required(),
-  location: Joi.string().min(4).trim().required(),
-  description: Joi.string().trim().min(4).allow('', null),
 };
