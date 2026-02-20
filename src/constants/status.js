b67b5c1726a2ae0c73e251a66aa9671c59c332f3
Iryna-M-html <ivmohnataya@gmail.com>
2026-02-20 14:21:09 +0200

    CHANGED STATUS

--- src/constants/status.js
+++ src/constants/status.js
@@ -1,5 +1,5 @@
 export const USER_STATUS = {
   ACTIVE: 'active', // Користувач працює
   INACTIVE: 'inactive', // Тимчасово вимкнено (пауза)
-  DISABLED: 'disabled', // Звільнений (повний стоп)
+  DEACTIVATED: 'deactivated', // Звільнений (повний стоп)
 };
