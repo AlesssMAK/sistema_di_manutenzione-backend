bc392f3da89800260f72e7b7a93034b386a34a47
Iryna-M-html <ivmohnataya@gmail.com>
2026-03-19 12:15:24 +0200

    create controller for MaintenanceWorker

--- src/controllers/maintenanceWorkerController.js
+++ src/controllers/maintenanceWorkerController.js
@@ -1,3 +1,4 @@
+import { Fault } from '../models/fault.js';
 import { User } from '../models/user.js';
 
 export const getAllMaintenanceWorker = async (req, res) => {
@@ -11,4 +12,39 @@ export const getAllMaintenanceWorker = async (req, res) => {
   });
 };
 
-export const addFaultByMaintenanceWorker = async (req, res) => {};
+export const addFaultByMaintenanceWorker = async (req, res) => {
+  try {
+    const { faultId, statusfault, commentMaintenanceWorker } = req.body;
+    const maintenanceWorkerId = req.user?._id;
+    const maintenanceWorkerName = req.user?.name || 'Монтер';
+    const fault = await Fault.findById(faultId);
+
+    if (!fault) {
+      return res.status(404).json({ message: 'Несправність не знайдена' });
+    }
+
+    const updateData = {
+      faultId,
+      statusfault,
+      commentMaintenanceWorker,
+      maintenanceWorkerId,
+    };
+    fault.history.push({
+      action: 'updated_by_maintenanceWorker',
+      userId: maintenanceWorkerId,
+      userName: maintenanceWorkerName,
+      changes: updateData,
+      timestamp: new Date(),
+    });
+
+    Object.assign(fault, updateData);
+
+    await fault.save();
+    return res.status(200).json(fault);
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Помилка при оновленні несправності монтером',
+      error: error.message,
+    });
+  }
+};
