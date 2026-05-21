3ca583e03de7d69825aa165f89d55ed88bb63cb2
Alexey <makovii88@gmail.com>
2026-05-22 00:18:11 +0200

    feat: update plant and plant part validation

--- src/validations/plantValidation.js
+++ src/validations/plantValidation.js
@@ -3,10 +3,10 @@ import { STATUS } from '../constants/status.js';
 
 export const createPlantSchema = {
   [Segments.BODY]: Joi.object({
-    namePlant: Joi.string().trim().min(4).required(),
-    code: Joi.string().trim().min(4).required(),
-    location: Joi.string().min(4).trim().required(),
-    description: Joi.string().trim().min(4).allow('', null),
+    namePlant: Joi.string().trim().required(),
+    code: Joi.string().trim().required(),
+    location: Joi.string().trim().required(),
+    description: Joi.string().trim().allow('', null),
     status: Joi.string()
       .valid(...Object.values(STATUS))
       .default(STATUS.ACTIVE),
